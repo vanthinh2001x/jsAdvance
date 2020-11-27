30f9c041cf66e02dad97fa01a221f361b67943fb
pham van thinh <phamvanthinh2001x@gmail.com>
2020-11-27 21:42:32 +0700

    spread p2

--- spread.js
+++ spread.js
@@ -1,3 +1,16 @@
 let a = [2,3,4];
 let b = [1,...a,5];
 console.log(b);
+
+const obj1 = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: {e:10}
+}
+let obj2 = {
+    ...obj1,
+    e: 100
+}
+obj2.d.e = 11;
+console.log(obj1, obj2);
